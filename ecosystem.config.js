5265555bb3616d21574b77f482f5fff20409617c
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-11 01:35:34 +0600

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'course-landing',
       port: '3000',
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       instances: 'max',
       script: './.output/server/index.mjs'
     }
