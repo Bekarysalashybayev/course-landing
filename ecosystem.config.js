f4a098df4163f67d37c3db3016194ac5f68ab7e1
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-11 01:15:46 +0600

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'course-landing', // Replace with your app's name
-      script: 'node', // Use 'node' to run a Node.js script
-      args: 'dist/server/index.js', // Path to the Nuxt 3 server entry file
-      instances: 'max', // You can adjust the number of instances
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
-};
+      name: 'course-landing',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
