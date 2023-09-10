73bc03ce6cf414f510c5dbf1061bb5a623f638f8
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-11 00:59:46 +0600

    pm2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    port: 5001
+  },
   devtools: { enabled: false },
   css: ['~/assets/css/main.css', "~/assets/scss/base.scss"],
 
