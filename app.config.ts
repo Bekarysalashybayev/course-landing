349da0316b9b1cfcfab7f97b65b19a26a73b9c5f
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-15 22:36:16 +0600

    head links

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+    title: "Testhub",
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/svg+xml',
+            href: '/favicon.svg'
+        }
+    ],
+})
