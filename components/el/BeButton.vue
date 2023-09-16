ca25f7b00e36fbafd6bc70f4d3d2b589629c21d0
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-16 14:47:11 +0600

    fix

--- components/el/BeButton.vue
+++ components/el/BeButton.vue
@@ -40,42 +40,5 @@ const classes = computed(() => {
   &:hover {
     opacity: .8;
   }
-
-  &.light {
-    color: $color-4;
-    background-color: $color-5;
-  }
-
-  &.primary {
-    color: $color-5;
-    background-color: $primary;
-  }
-
-  &.primary-1 {
-    color: $color-5;
-    background: linear-gradient($color-1, $color-6);
-  }
-
-  &.yellow {
-    color: $color-5;
-    background: linear-gradient($color-14, $color-16)
-  }
-
-  &.rounded {
-    border-radius: 8rem;
-  }
-
-  &.small {
-    font-size: 1.5rem;
-    padding: .6rem 1.5rem;
-  }
-
-  &.medium {
-    padding: 1rem 3.5rem;
-  }
-
-  &.large {
-    padding: 2rem 4rem;
-  }
 }
 </style>
