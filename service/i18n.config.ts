0692b8c6ad8a1cb003a0b444f161b33dc86351b6
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-23 16:49:50 +0600

    i18n

--- service/i18n.config.ts
+++ service/i18n.config.ts
@@ -2,7 +2,7 @@ import kk from '../locales/kk.json'
 import ru from '../locales/ru.json'
 
 export default defineI18nConfig(() => ({
-    legacy: false,
+    legacy: true,
     fallbackLocale: 'kz',
     messages: {
         kz: kk,
