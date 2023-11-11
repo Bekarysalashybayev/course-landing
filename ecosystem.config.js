4ba01a443c8b0f93b215d61bfb7feedbd2deb880
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-11-11 18:09:36 +0600

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'course-landing',
-      port: '3000',
+      port: '3040',
       exec_mode: 'fork',
       instances: 'max',
       script: '.output/server/index.mjs'
